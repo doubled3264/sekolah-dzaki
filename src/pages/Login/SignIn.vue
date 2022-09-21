488e88b07169cb5d1eeeec72099dd676fbad71fd
doubled3264 <dede.doubled3264@gmail.com>
2022-09-21 08:39:46 +0700

    no info

--- src/pages/Login/SignIn.vue
+++ src/pages/Login/SignIn.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 import Swal from 'sweetalert2'
 import { loginScheme } from '../../utils/validation/login.scheme'
 import CustomInput from '../../components/CustomInput.vue'
@@ -8,110 +9,96 @@ import CustomButton from '../../components/CustomButton.vue'
 import _ from 'lodash'
 
 const store = useStore()
+const router = useRouter()
 const pegawai = ref({
-   email: {
-      value: '',
-      isValid: false,
-      errorMessage: '',
-   },
-   password: {
-      value: '',
-      isValid: false,
-      errorMessage: '',
-   },
+  email: {
+    value: '',
+    isValid: false,
+    errorMessage: '',
+  },
+  password: {
+    value: '',
+    isValid: false,
+    errorMessage: '',
+  },
 })
 let pegawaiKey = null
 // -------------- function --------------
 function clearForm() {
-   _.forEach(pegawaiKey, (key) => {
-      pegawai.value[key].value = ''
-      pegawai.value[key].isValid = false
-   })
+  _.forEach(pegawaiKey, (key) => {
+    pegawai.value[key].value = ''
+    pegawai.value[key].isValid = false
+  })
 }
 async function validateInput(field) {
-   await loginScheme
-      .validateAt(field + '.value', pegawai.value)
-      .then((result) => {
-         pegawai.value[field].isValid = true
-      })
-      .catch((err) => {
-         pegawai.value[field].errorMessage = err.message
-         pegawai.value[field].isValid = false
-      })
+  await loginScheme
+    .validateAt(field + '.value', pegawai.value)
+    .then((result) => {
+      pegawai.value[field].isValid = true
+    })
+    .catch((err) => {
+      pegawai.value[field].errorMessage = err.message
+      pegawai.value[field].isValid = false
+    })
 }
 function validateBeforeSubmit() {
-   let validCount = 0
-   _.forEach(pegawaiKey, (key) => {
-      if (pegawai.value[key].isValid) {
-         validCount++
-      }
-   })
+  let validCount = 0
+  _.forEach(pegawaiKey, (key) => {
+    if (pegawai.value[key].isValid) {
+      validCount++
+    }
+  })
 
-   if (validCount == pegawaiKey.length) {
-      submitAction()
-   } else {
-      Swal.fire({
-         icon: 'warning',
-         text: 'terdapat form yang belum terisi',
-         confirmButtonText: 'tutup',
-      })
-   }
+  if (validCount == pegawaiKey.length) {
+    submitAction()
+  } else {
+    Swal.fire({
+      icon: 'warning',
+      text: 'terdapat form yang belum terisi',
+      confirmButtonText: 'tutup',
+    })
+  }
 }
 async function submitAction() {
-   store.dispatch('auth/signIn', {
-      email: pegawai.value.email.value,
-      password: pegawai.value.password.value,
-   })
+  await store.dispatch('auth/signIn', {
+    email: pegawai.value.email.value,
+    password: pegawai.value.password.value,
+  })
+    .then((response) => {
+      router.push({ name: 'siswa' })
+    })
 }
 // -------------- cyclehook --------------
 onMounted(() => {
-   pegawaiKey = _.keys(pegawai.value)
+  pegawaiKey = _.keys(pegawai.value)
 })
 </script>
 
 <template>
-   <div class="row flex-col items-center px-8">
-      <h2>Silahkan login terlebih dahulu</h2>
-      <form>
-         <div class="row">
-            <div class="input-field w-full mb-4">
-               <CustomInput
-                  type="email"
-                  label="email"
-                  :error-state="{
-                     isError: !pegawai.email.isValid,
-                     message: pegawai.email.errorMessage,
-                  }"
-                  v-model:input-value="pegawai.email.value"
-                  @validate-input="validateInput('email')"
-               />
-            </div>
-         </div>
-         <div class="row">
-            <div class="input-field w-full mb-4">
-               <CustomInput
-                  type="password"
-                  label="password"
-                  :error-state="{
-                     isError: !pegawai.password.value,
-                     message: pegawai.password.errorMessage,
-                  }"
-                  v-model:input-value="pegawai.password.value"
-                  @validate-input="validateInput('password')"
-               />
-            </div>
-         </div>
-         <div class="form-nav">
-            <CustomButton
-               title="Masuk"
-               variant="solid"
-               color="primary"
-               size="sm"
-               block
-               @button-action="validateBeforeSubmit"
-            />
-            <p>Gagal masuk karena lupa passowrd ? <span>Klik di sini</span></p>
-         </div>
-      </form>
-   </div>
+  <div class="row flex-col items-center px-8">
+    <h2>Silahkan login terlebih dahulu</h2>
+    <form>
+      <div class="row">
+        <div class="input-field w-full mb-4">
+          <CustomInput type="email" label="email" :error-state="{
+             isError: !pegawai.email.isValid,
+             message: pegawai.email.errorMessage,
+          }" v-model:input-value="pegawai.email.value" @validate-input="validateInput('email')" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field w-full mb-4">
+          <CustomInput type="password" label="password" :error-state="{
+             isError: !pegawai.password.value,
+             message: pegawai.password.errorMessage,
+          }" v-model:input-value="pegawai.password.value" @validate-input="validateInput('password')" />
+        </div>
+      </div>
+      <div class="form-nav">
+        <CustomButton title="Masuk" variant="solid" color="primary" size="sm" block
+          @button-action="validateBeforeSubmit" />
+        <p>Gagal masuk karena lupa passowrd ? <span>Klik di sini</span></p>
+      </div>
+    </form>
+  </div>
 </template>
--- src/pages/Pembayaran/BasePembayaran.vue
+++ src/pages/Pembayaran/BasePembayaran.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
+import { VueGoodTable } from 'vue-good-table-next'
 import Sidebar from '../../components/layouts/sidebar/Sidebar.vue'
 import CustomHeader from '../../components/layouts/header/CustomHeader.vue'
 import CustomButton from '../../components/CustomButton.vue'
@@ -8,29 +9,90 @@ import CustomButton from '../../components/CustomButton.vue'
 const store = useStore()
 const overlayIsActive = ref(false)
 
+const tableData = ref({
+  columns: [
+    {
+      label: 'No',
+      field: 'nomor',
+      type: 'number',
+      thClass: 'text-left !w-16',
+      tdClass: 'text-left !pl-5',
+    },
+    {
+      label: 'ID Pembayaran',
+      field: 'id',
+      thClass: 'text-left',
+    },
+    /* { */
+    /*   label: 'Tanggal Lahir', */
+    /*   field: 'tanggal_lahir', */
+    /*   type: 'date', */
+    /*   dateInputFormat: 'yyyy-MM-dd', */
+    /*   dateOutputFormat: 'MMM do yy', */
+    /*   thClass: 'text-left', */
+    /* }, */
+    {
+      label: 'ID Siswa',
+      field: 'id_siswa',
+      thClass: 'text-left',
+    },
+    {
+      label: 'Pembayaran',
+      field: 'nama_pembayaran',
+      thClass: 'text-left',
+    },
+    {
+      label: 'Nominal',
+      field: 'nominal',
+      thClass: 'text-left',
+    },
+    {
+      label: 'Status',
+      field: 'status',
+      thClass: 'text-left',
+    },
+    {
+      label: 'Metode Pembayaran',
+      field: 'metode_pembayaran',
+      thClass: 'text-left',
+    },
+  ],
+  rows: [
+    /* { id: 2, name: 'Jane', age: 24, createdAt: '2011-10-31', score: 0.03343 }, */
+    /* { id: 3, name: 'Susan', age: 16, createdAt: '2011-10-30', score: 0.03343 }, */
+    /* { id: 4, name: 'Chris', age: 55, createdAt: '2011-10-11', score: 0.03343 }, */
+    /* { id: 5, name: 'Dan', age: 40, createdAt: '2011-10-21', score: 0.03343 }, */
+    /* { id: 6, name: 'John', age: 20, createdAt: '2011-10-31', score: 0.03343 }, */
+  ],
+})
 function activeModal() {
-   overlayIsActive.value = !overlayIsActive.value
+  overlayIsActive.value = !overlayIsActive.value
 }
+// -------------- computed --------------
+const getWindowSize = computed(() => {
+  if (store.getters['windowProp/getWidth'] >= 1024) {
+    return 'sm'
+  }
+  return 'lg'
+})
+// -------------- cyclehook --------------
 onMounted(() => {
-   overlayIsActive.value = true
-   store.commit('sidebar/setActivePage', 'pembayaran')
+  overlayIsActive.value = true
+  store.commit('sidebar/setActivePage', 'pembayaran')
 })
 </script>
 <template>
-   <div class="content">
-      <div class="wrapper">
-         <CustomHeader />
-         <div class="table-data">
-            <div class="table-data__title">
-               <h4>data pembayaran</h4>
-               <CustomButton
-                  title="tambah data"
-                  color="primary"
-                  @button-action="activeModal"
-               />
-            </div>
-         </div>
+  <div class="content">
+    <div class="wrapper">
+      <CustomHeader />
+      <div class="table-data">
+        <div class="table-data__title">
+          <h4>data pembayaran</h4>
+          <CustomButton title="tambah data" color="primary" @button-action="activeModal" :size="getWindowSize"/>
+        </div>
+        <vue-good-table :columns="tableData.columns" :rows="tableData.rows" styleClass="vgt-table stiped" />
       </div>
-   </div>
-   <Sidebar />
+    </div>
+  </div>
+  <Sidebar />
 </template>
