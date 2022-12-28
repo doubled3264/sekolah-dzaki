861ff4e15d730fdd59b0725fc71b6e36fc81a02a
doubled3264 <dede.doubled3264@gmail.com>
2022-12-28 18:02:47 +0700

    add input type files

--- src/pages/Artikel/ArticleAdd.vue
+++ src/pages/Artikel/ArticleAdd.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import { arrowRight, imageAdd, textAdd, disket } from '../../utils/svg-var'
+import * as tinyMceCustomIcon from '../../utils/tinyMceCustomIcon'
 import { artikelSchema } from '../../utils/validation/artikel.schema'
 import Editor from '@tinymce/tinymce-vue'
 import CustomIcon from '../../components/CustomIcon.vue'
@@ -9,136 +10,175 @@ import CustomButton from '../../components/CustomButton.vue'
 import CustomInput from '../../components/form/CustomInput.vue'
 import CustomModalOverlay from '../../components/CustomModalOverlay.vue'
 import ArticleChooseItem from '../../components/modal/ArticleAddItem.vue'
+import CustomImageInput from '../../components/form/CustomImageInput.vue'
 
 const props = defineProps({
-  parentItem: String,
-  childItem: String,
+   parentItem: String,
+   childItem: String,
 })
 const store = useStore()
 const artikel = ref({
-  title: '',
-  item: [],
+   title: '',
+   item: [],
 })
 
 const errorState = ref({
-  title: {
-    isError: true,
-    message: '',
-  },
+   title: {
+      isError: true,
+      message: '',
+   },
 })
 const modal = ref({
-  chooseItem: false,
-  textEditor: false,
-  imageEditor: false,
+   chooseItem: false,
+   textEditor: false,
+   imageEditor: false,
 })
 const tinyMceOptions = ref({
-  apiKey: 'emcqski0nlymetzrqpc6wq5pg0klreddh5chpfs8qipwf9jk',
-  init: {
-    menubar: false,
-    toolbar: 'undo redo | blocks | bold italic |  bullist numlist | save',
-    plugins: 'save lists',
-    save_onsavecallback: () => {
-      pushArticle()
-    },
-  },
-  contentValue: '',
+   apiKey: 'emcqski0nlymetzrqpc6wq5pg0klreddh5chpfs8qipwf9jk',
+   init: {
+      menubar: false,
+      toolbar:
+         'undo redo | blocks | bold italic |  bullist numlist | customSave',
+      plugins: 'save lists',
+      save_onsavecallback: () => {
+         pushTextArticle()
+      },
+      setup: (editor) => {
+         editor.ui.registry.addIcon(
+            'customSaveIcon',
+            tinyMceCustomIcon.save('18')
+         )
+         editor.ui.registry.addButton('customSave', {
+            icon: 'customSaveIcon',
+            onAction: function (_) {
+               pushTextArticle()
+            },
+         })
+      },
+   },
+   contentValue: '',
 })
 onMounted(() => {
-  store.commit('sidebar/setAllToNormal')
-  store.commit('sidebar/setActiveParent', props.parentItem)
-  store.commit('sidebar/setActiveChild', {
-    parent: props.parentItem,
-    itemToActive: props.childItem,
-  })
+   store.commit('sidebar/setAllToNormal')
+   store.commit('sidebar/setActiveParent', props.parentItem)
+   store.commit('sidebar/setActiveChild', {
+      parent: props.parentItem,
+      itemToActive: props.childItem,
+   })
 })
 
 function toggleModal(modalName) {
-  modal.value[modalName] = !modal.value[modalName]
+   modal.value[modalName] = !modal.value[modalName]
 }
 
-function pushArticle() {
-  /* console.log(tinyMceOptions.value.contentValue) */
-  artikel.value.item.push(tinyMceOptions.value.contentValue)
-  tinyMceOptions.value.contentValue = ''
-  toggleModal('textEditor')
-  console.log(artikel.value.item)
+function pushTextArticle() {
+   if (!tinyMceOptions.value.contentValue.length === 0) {
+      artikel.value.item.push({
+         type: 'text',
+         value: tinyMceOptions.value.contentValue,
+      })
+      tinyMceOptions.value.contentValue = ''
+      toggleModal('textEditor')
+      console.log(artikel.value.item)
+   }
+}
+
+function pushImageArticle(imageValue) {
+   artikel.value.item.push({
+      type: 'image',
+      value: imageValue,
+   })
+   console.log(artikel.value.item)
 }
 /**
  * validate input when event triggered
  * @param {string} siswa object key
  */
 async function validateInput(field) {
-  await artikelSchema
-    .validateAt(field, artikel.value)
-    .then(() => {
-      errorState.value[field].isError = false
-    })
-    .catch((err) => {
-      errorState.value[field].isError = true
-      errorState.value[field].message = err.message
-    })
+   await artikelSchema
+      .validateAt(field, artikel.value)
+      .then(() => {
+         errorState.value[field].isError = false
+      })
+      .catch((err) => {
+         errorState.value[field].isError = true
+         errorState.value[field].message = err.message
+      })
 }
 
 function addTextItem() {
-  toggleModal('chooseItem')
-  toggleModal('textEditor')
+   toggleModal('chooseItem')
+   toggleModal('textEditor')
 }
 function addPictureAction() {
-  toggleModal('chooseItem')
-  console.log('addPictureAction run')
+   toggleModal('chooseItem')
+   console.log('addPictureAction run')
 }
 </script>
 <template>
-  <div class="content">
-    <div class="content__inner">
-      <div class="content__head">
-        <div class="content__path">
-          <span class="cursor-pointer" @click="cancelAction">artikel</span>
-          <CustomIcon :svg-icon="arrowRight" width="10" />
-          <span>tambah data artikel baru</span>
-        </div>
-        <div class="content__title">
-          <h3>tambah data artikel baru</h3>
-        </div>
+   <div class="content">
+      <div class="content__inner">
+         <div class="content__head">
+            <div class="content__path">
+               <span class="cursor-pointer" @click="cancelAction">artikel</span>
+               <CustomIcon :svg-icon="arrowRight" width="10" />
+               <span>tambah data artikel baru</span>
+            </div>
+            <div class="content__title">
+               <h3>tambah data artikel baru</h3>
+            </div>
+         </div>
+         <div class="content__body">
+            <div class="card artikel-add">
+               <div class="card__head">
+                  <div class="card__title">
+                     <h3>Masukan Data Artikel</h3>
+                  </div>
+                  <div class="card__nav">
+                     <div
+                        class="icon__wrapper"
+                        @click="toggleModal('textEditor')"
+                     >
+                        <CustomIcon :svg-icon="textAdd" />
+                     </div>
+                     <CustomImageInput @push-image="pushImageArticle" />
+                     <div class="icon__wrapper">
+                        <CustomIcon :svg-icon="disket" />
+                     </div>
+                  </div>
+               </div>
+               <div class="card__body">
+                  <form>
+                     <div>
+                        <CustomInput
+                           type="text"
+                           label="Judul"
+                           placeholder="Masukan judul artikel.."
+                           v-model:input-value="artikel.nama"
+                           :error-state="errorState.nama"
+                           @validate-input="validateInput('title')"
+                        />
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
       </div>
-      <div class="content__body">
-        <div class="card artikel-add">
-          <div class="card__head">
-            <div class="card__title">
-              <h3>Masukan Data Artikel</h3>
-            </div>
-            <div class="card__nav">
-              <div class="icon__wrapper" @click="toggleModal('textEditor')">
-                <CustomIcon :svg-icon="textAdd" />
-              </div>
-              <div class="icon__wrapper">
-                <CustomIcon :svg-icon="imageAdd" />
-              </div>
-              <div class="icon__wrapper">
-                <CustomIcon :svg-icon="disket" />
-              </div>
-            </div>
-          </div>
-          <div class="card__body">
+   </div>
+   <teleport to="#modal">
+      <CustomModalOverlay
+         v-show="modal.textEditor"
+         @close-modal="toggleModal('textEditor')"
+      >
+         <div class="tinymce__wrapper">
             <form>
-              <div>
-                <CustomInput type="text" label="Judul" placeholder="Masukan judul artikel.."
-                  v-model:input-value="artikel.nama" :error-state="errorState.nama"
-                  @validate-input="validateInput('title')" />
-              </div>
+               <Editor
+                  :api-key="tinyMceOptions.apiKey"
+                  :init="tinyMceOptions.init"
+                  v-model="tinyMceOptions.contentValue"
+               />
             </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <teleport to="#modal">
-    <CustomModalOverlay v-show="modal.textEditor" @close-modal="toggleModal('textEditor')">
-      <div class="tinymce__wrapper">
-        <form>
-          <Editor :api-key="tinyMceOptions.apiKey" :init="tinyMceOptions.init" v-model="tinyMceOptions.contentValue" />
-        </form>
-      </div>
-    </CustomModalOverlay>
-  </teleport>
+         </div>
+      </CustomModalOverlay>
+   </teleport>
 </template>
---
+++ src/utils/tinyMceCustomIcon.js
@@ -0,0 +1,23 @@
+import { disket } from './svg-var'
+export const save = (width) => {
+  return `
+
+   <svg
+      width="${width}"
+      height="${width}"
+      viewBox="0 0 24 24"
+      xml:space="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+   >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <g ref="icon">
+         <path
+            d="${disket}"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="0.09"
+         />
+      </g>
+   </svg>
+  `
+}
